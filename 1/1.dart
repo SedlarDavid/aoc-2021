b47a8c06f5565bdd9f23ceaefd8ae6530e37f76d
SedlarDavid <sedlardavid.cz@gmail.com>
2021-12-01 13:16:00 +0100

    1 - part 1

---
+++ 1/1.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final input = new File('1/input.txt');
+
+  final data = input
+      .readAsLinesSync()
+      .map((l) => int.tryParse(l))
+      .toList(growable: false);
+
+  var increaseTime = 0;
+
+  var lastMeasurement = 0;
+  for (var i = 0; i < data.length; i++) {
+    final val = data[i];
+    if (val == null) continue;
+    if (i == 0) {
+      lastMeasurement = val;
+      continue;
+    }
+    if (val > lastMeasurement) {
+      increaseTime++;
+    }
+    lastMeasurement = val;
+    continue;
+  }
+
+  print('IC count: $increaseTime');
+}
