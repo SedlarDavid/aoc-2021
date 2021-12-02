40429130f4a7e6c3b2ce66703d940ac6f05af71c
SedlarDavid <sedlardavid.cz@gmail.com>
2021-12-02 10:26:02 +0100

    2 - part 1

---
+++ 2/2.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final input = new File('2/input.txt');
+
+  partOne(input);
+}
+
+void partOne(File input) {
+  final data = input.readAsLinesSync().map((l) {
+    final split = l.split(' ');
+    final course = split.first;
+    final value = int.tryParse(split.last) ?? 0;
+    return MapEntry<String, int>(course, value);
+  }).toList(growable: false);
+
+  int f = 0;
+  int d = 0;
+
+  for (var i = 0; i < data.length; i++) {
+    final entry = data[i];
+
+    switch (entry.key) {
+      case 'forward':
+        f += entry.value;
+        break;
+      case 'up':
+        d -= entry.value;
+        break;
+      case 'down':
+        d += entry.value;
+        break;
+      default:
+        break;
+    }
+  }
+
+  final position = f * d;
+
+  print('F: $f, D: $f, POSITION: $position');
+}
